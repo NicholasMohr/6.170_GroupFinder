aa7b4bec94dcf30192182bbd0b0ba4ef2bd1a9d5
Danielle Man <daniman0808@gmail.com>
2014-10-16 22:29:18 -0400

    defined schema

--- models/projects.js
+++ models/projects.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 var projectSchema = mongoose.Schema({
-	
+	name: String,
+	end_date: Date,
+	users: [{type: String, ref: 'User'}]
 });
 
 var Project = mongoose.model('Project', projectSchema);
--- models/users.js
+++ models/users.js
@@ -1,9 +1,26 @@
 var mongoose = require('mongoose');
 
-var userSchema = mongoose.Schema({
-	
-});
+var user_schema = mongoose.Schema({
+	authentication: {
+		name: String,
+		username: String,
+		password: String
+	},
+	projects: [{ type: String, ref: "Projects"}],
+	info: {
+		location: String,
+		availibility: [String],
+		skills: [String],
+		workstyle : {
+			grade: Number,
+			interaction: Number,
+			dedication: Number,
+			timing: Number
+		}
+	},
 
-var User = mongoose.model('User', userSchema);
+var User = mongoose.model('User', user_schema);
 
 module.exports = User;
+
+Yeah it's like three separate clicks
