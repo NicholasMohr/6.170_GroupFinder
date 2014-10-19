c9e98f54491ea9184f7bfd63f61bf856bc4b385c
Marissa Stephens <marissas@mit.edu>
2014-10-19 17:42:59 -0400

    fixed how I access the session user in filter

--- routes/projects.js
+++ routes/projects.js
@@ -31,7 +31,7 @@ router.get('/:project_name/users', function(req, res) {
 });
 
 router.get('/:project_name/users/filter', function(req, res) {
-	var currentUser=req.session.currentUser; //TODO: CHECK THAT THIS IS ACTUALLY HOW IT'S DONE
+	var currentUsername=req.session.user; 
 	var name=req.param('project_name');
 	var location=req.param('location')?req.param('location') :1;
 	var availability=req.param('availability')?req.param('availability') :1;	
@@ -42,58 +42,62 @@ router.get('/:project_name/users/filter', function(req, res) {
 	var skills=req.param('skills')?req.param('skills') :1;
 	var skillset=req.param('skillset')?req.param('skillset') :[];		
 	data.Project.find({name:name}).exec(function(err,doc){	
-		//THROW PROJECT NOT FOUND ERROR
 		if(err || doc.length==0){   
             res.send("That project could not be found");
 		}
 		var usernames=doc[0].users;
-		data.User.find({name:{$in: usernames}}, function(errs,docs){
-			//THROW USER NOT FOUND ERROR
+		data.User.find({name:currentUsername}).exec(function(err,doc){
 			if(errs || docs.length==0){   
-            	res.send("The project could not find any users");
+				res.send("The project could not find the user");
 			}
-			docs.forEach(function(user){
-				var score=0;
-				// Add 1 if the location is the same. Otherwise add 0
-				if(user.location==currentUser.location){
-					score+= location
-				}
-				// number of same hours availible over total number of hours current user is available
-				// times the user-inputted weight
-				if(currentUser.availibility.length!=0){
-				var result = currentUser.availibility.filter(function(c) {
-					return user.availibility.indexOf(c) !== -1;
+			var currentUser=doc[0];
+			data.User.find({name:{$in: usernames}}).exec(function(errs,docs){
+				docs.forEach(function(user){
+					var score=0;
+					// Add 1 if the location is the same. Otherwise add 0
+					if(user.location==currentUser.location){
+						score+= location
+					}
+					// number of same hours availible over total number of hours current user is available
+					// times the user-inputted weight
+					if(currentUser.availibility.length!=0){
+					var result = currentUser.availibility.filter(function(c) {
+						return user.availibility.indexOf(c) !== -1;
+					});
+						score+=(result.length*availability)/currentUser.availibility.length
+					}
+					// for grade, interaction, dedication, and timing the score is 1 minus the difference 
+					// times the user-inputed weight
+					if(currentUser.grade){
+						score+=score+ (1-Math.abs(currentUser.grade-user.grade))*grade;
+					}
+					if(currentUser.interaction){
+						score+=(1-Math.abs(currentUser.interaction-user.interaction))*interaction;
+					}
+					if(currentUser.dedication){
+						score+= (1-Math.abs(currentUser.dedication-user.dedication))*dedication;
+					}
+					if(currentUser.timing){
+						score+=(1-Math.abs(currentUser.timing-user.timing))*timing;
+					}
+					// number of matched requested skills divided by the total number of requested skills
+					// times the user-inputted weight
+					if(skillset.length!=0){
+					var result = skillset.filter(function(c) {
+						return user.skillset.indexOf(c) !== -1;
+					});
+						score+=(result.length*skills)/skillset.length
+					}
+					// add score to user
+					user.score=score;
 				});
-					score+=(result.length*availability)/currentUser.availibility.length
-				}
-				// for grade, interaction, dedication, and timing the score is 1 minus the difference 
-				// times the user-inputed weight
-				if(currentUser.grade){
-					score+=score+ (1-Math.abs(currentUser.grade-user.grade))*grade;
-				}
-				if(currentUser.interaction){
-					score+=(1-Math.abs(currentUser.interaction-user.interaction))*interaction;
-				}
-				if(currentUser.dedication){
-					score+= (1-Math.abs(currentUser.dedication-user.dedication))*dedication;
-				}
-				if(currentUser.timing){
-					score+=(1-Math.abs(currentUser.timing-user.timing))*timing;
-				}
-				// number of matched requested skills divided by the total number of requested skills
-				// times the user-inputted weight
-				if(skillset.length!=0){
-				var result = skillset.filter(function(c) {
-					return user.skillset.indexOf(c) !== -1;
-				});
-					score+=(result.length*skills)/skillset.length
-				}
-				// add score to user
-				user.score=score;
+				// return users sorted by ascending scores
+				res.json(users.sort(function(a, b) {return a.score - b.score;}));
 			});
-			// return users sorted by ascending scores
-			res.json(users.sort(function(a, b) {return a.score - b.score;}));
+			
 		});
+		
+
 	});
 	
 });
