f8a1a6f0575947ece418c794c7ee95536b02fb1f
Marissa Stephens <marissas@mit.edu>
2014-10-20 00:15:45 -0400

    project stuff

--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/users');
+var Project = require('../models/projects');
+var utils = require('../utils/utils');
 
 router.get('/', function(req, res) {
   	//TODO: RETURN LIST OF PROJECTS
-  	data.Project.find({}).exec(function(e,projects){
+  	Project.find({}).exec(function(e,projects){
   		res.json(projects);
   	})
   	//res.render('index', { title: 'Project Request'});
@@ -12,9 +15,9 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
 
 	//TODO: make sure there is not already a project
-	var newProject = new data.Project({
-	  name: req.param('name', ''),
-	  end_date: req.param('end_date', ''),//should be some nice date representation
+	var newProject = new Project({
+	  name: req.query.name,
+	  end_date: req.query.end_date //should be some nice date representation
 	})
 	newProject.save(function(err){
 		if(err){
@@ -25,78 +28,87 @@ router.post('/', function(req, res) {
 }); 
 
 router.get('/:project_name/users', function(req, res) {
-  	data.Project.find({"name" : req.params.project_name}).populate('users').exec(function(err, docs){
+  	Project.find({"name" : req.params.project_name}).populate('users').exec(function(err, docs){
   		res.json(docs[0].users);
   	});
 });
 
 router.get('/:project_name/users/filter', function(req, res) {
-	var currentUsername=req.session.user; 
+	var userID=req.session.passport.user; 
 	var name=req.param('project_name');
-	var location=req.param('location')?req.param('location') :1;
-	var availability=req.param('availability')?req.param('availability') :1;	
-	var grade=req.param('grade')?req.param('grade') :1;
-	var interaction=req.param('interaction')?req.param('interaction') :1;	
-	var dedication=req.param('dedication')?req.param('dedication') :1;
-	var timing=req.param('timing')?req.param('timing') :1;	
-	var skills=req.param('skills')?req.param('skills') :1;
-	var skillset=req.param('skillset')?req.param('skillset') :[];		
-	data.Project.find({name:name}).exec(function(err,doc){	
-		if(err || doc.length==0){   
-            res.send("That project could not be found");
+	var location=req.query.location?req.query.location :1;
+	var availability=req.query.availability?req.query.availability :1;	
+	var grade=req.query.grade?req.query.grade :1;
+	var interaction=req.query.interaction?req.query.interaction :1;	
+	var dedication=req.query.dedication?req.query.dedication :1;
+	var timing=req.query.timing?req.query.timing :1;	
+	var skills=req.query.skills?req.query.skills :1;
+	var skillset=req.query.skillset?req.query.skillset :[];		
+	Project.findOne({name:name},function(err,project){
+		if(err||project==null){   
+            utils.sendErrResponse(res, 403, 'The project could not be found.');
 		}
-		var usernames=doc[0].users;
-		data.User.find({name:currentUsername}).exec(function(err,doc){
-			if(errs || docs.length==0){   
-				res.send("The project could not find the user");
-			}
-			var currentUser=doc[0];
-			data.User.find({name:{$in: usernames}}).exec(function(errs,docs){
-				docs.forEach(function(user){
-					var score=0;
-					// Add 1 if the location is the same. Otherwise add 0
-					if(user.location==currentUser.location){
-						score+= location
-					}
-					// number of same hours availible over total number of hours current user is available
-					// times the user-inputted weight
-					if(currentUser.availibility.length!=0){
-					var result = currentUser.availibility.filter(function(c) {
-						return user.availibility.indexOf(c) !== -1;
+		else{
+			var projectID=project.id;
+			var usernames=doc.users;
+			User.findOne({id:userID} ,function(err,user){
+				if(err||user==null){   
+					utils.sendErrResponse(res, 403, 'The user could not be found');
+				}
+				else{
+					var currentUser=user;
+					User.find({name:{$in: usernames}},function(errs,docs){
+						if(errs){
+							utils.sendErrResponse(res, 500, 'An unknown error occurred.');
+						}
+						else{
+								
+							docs.forEach(function(user){
+								var score=0;
+								// Add 1 if the location is the same. Otherwise add 0
+								if(user.location==currentUser.location){
+									score+= location
+								}
+								// number of same hours availible over total number of hours current user is available
+								// times the user-inputted weight
+								if(currentUser.availibility.length!=0){
+								var result = currentUser.availibility.filter(function(c) {
+									return user.availibility.indexOf(c) !== -1;
+								});
+									score+=(result.length*availability)/currentUser.availibility.length
+								}
+								// for grade, interaction, dedication, and timing the score is 1 minus the difference 
+								// times the user-inputed weight
+								if(currentUser.grade){
+									score+=score+ (1-Math.abs(currentUser.grade-user.grade))*grade;
+								}
+								if(currentUser.interaction){
+									score+=(1-Math.abs(currentUser.interaction-user.interaction))*interaction;
+								}
+								if(currentUser.dedication){
+									score+= (1-Math.abs(currentUser.dedication-user.dedication))*dedication;
+								}
+								if(currentUser.timing){
+									score+=(1-Math.abs(currentUser.timing-user.timing))*timing;
+								}
+								// number of matched requested skills divided by the total number of requested skills
+								// times the user-inputted weight
+								if(skillset.length!=0){
+								var result = skillset.filter(function(c) {
+									return user.skillset.indexOf(c) !== -1;
+								});
+									score+=(result.length*skills)/skillset.length
+								}
+								// add score to user
+								user.score=score;
+							});
+							// return users sorted by ascending scores
+							res.json(users.sort(function(a, b) {return a.score - b.score;}));
+						}
 					});
-						score+=(result.length*availability)/currentUser.availibility.length
-					}
-					// for grade, interaction, dedication, and timing the score is 1 minus the difference 
-					// times the user-inputed weight
-					if(currentUser.grade){
-						score+=score+ (1-Math.abs(currentUser.grade-user.grade))*grade;
-					}
-					if(currentUser.interaction){
-						score+=(1-Math.abs(currentUser.interaction-user.interaction))*interaction;
-					}
-					if(currentUser.dedication){
-						score+= (1-Math.abs(currentUser.dedication-user.dedication))*dedication;
-					}
-					if(currentUser.timing){
-						score+=(1-Math.abs(currentUser.timing-user.timing))*timing;
-					}
-					// number of matched requested skills divided by the total number of requested skills
-					// times the user-inputted weight
-					if(skillset.length!=0){
-					var result = skillset.filter(function(c) {
-						return user.skillset.indexOf(c) !== -1;
-					});
-						score+=(result.length*skills)/skillset.length
-					}
-					// add score to user
-					user.score=score;
-				});
-				// return users sorted by ascending scores
-				res.json(users.sort(function(a, b) {return a.score - b.score;}));
+				}
 			});
-			
-		});
-		
+		}
 
 	});
 	
@@ -123,7 +135,7 @@ router.delete('/:project_name/users', function(req, res) {
   	//TODO: delete the logged in user from the project
 
   	if(req.user){
-  		data.Project.find({"name" : req.params.project_name}).populate('users').exec(function(err, docs){
+  		Project.find({"name" : req.params.project_name}).populate('users').exec(function(err, docs){
   			docs.users.remove(req.user._id)
   			user.save(function(){
   				res.send('worked!');
@@ -137,7 +149,7 @@ router.delete('/:project_name/users', function(req, res) {
 
 router.delete('/:project_name', function(req, res) {
   	if(req.user){
-  		data.Project.remove({name:req.params.project_name});
+  		Project.remove({name:req.params.project_name});
   	}
   	else{
   		res.send('no session, make sure you\'re logged in!');
