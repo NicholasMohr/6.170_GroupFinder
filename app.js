5b3e61cea9a1d9abcf36f79390824dc9a94bce55
Marissa Stephens <marissas@mit.edu>
2014-10-19 22:10:07 -0400

    why isnt openshift working:

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var flash = require('connect-flash');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-var connection_string = 'localhost/groupfinder';
+var connection_string = 'localhost/nodetest1';
 
 // openshift dependencies
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
